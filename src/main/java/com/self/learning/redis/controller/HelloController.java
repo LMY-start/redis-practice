3c088fcdca4e688ba5d73b2bf9bdecc4aa83c36d
yangkaiguang <thoughtworks.yang@daimler.com>
2020-05-24 22:47:50 +0800

    [Kaiguang] use cache annotation

--- src/main/java/com/self/learning/redis/controller/HelloController.java
+++ src/main/java/com/self/learning/redis/controller/HelloController.java
@@ -1,18 +1,34 @@
 package com.self.learning.redis.controller;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CacheConfig(cacheNames = {"hello"})
 public class HelloController {
 
     private static final String NICE_TO_MEET_YOU = "nice to meet you";
+    private static final String NICE_TO_MEET_YOU_TOO = "nice to meet you, too";
 
+    @Cacheable(key = "#name")
     @GetMapping
-    public String sayHello() {
-        return NICE_TO_MEET_YOU;
+    public String sayHello(@RequestParam String name) {
+        System.out.println("not use cache in read");
+        return NICE_TO_MEET_YOU + ", " + name;
+    }
+
+    @CachePut(key = "#name")
+    @PostMapping
+    public String receiveHello(@RequestParam String name) {
+        System.out.println("always run to write cache");
+        return NICE_TO_MEET_YOU_TOO + ", " + name;
     }
 
 }
--- src/test/java/com/self/learning/redis/RedisTest.java
+++ src/test/java/com/self/learning/redis/RedisTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -14,8 +19,26 @@ public class RedisTest {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    private CacheManager cacheManager;
+
     @Test
     void should_set_value_success() {
-        stringRedisTemplate.opsForValue().set("KG", "handsome");
+        stringRedisTemplate.opsForValue().set("KG", "hello");
+    }
+
+    @Test
+    void should_execute_pipeline_success() {
+        List<Object> objects = stringRedisTemplate.executePipelined((RedisCallback<String>) connection -> {
+            IntStream.range(0, 3).forEach(index -> connection.set(String.valueOf(index).getBytes(), String.valueOf('A' + index).getBytes()));
+            IntStream.range(0, 3).forEach(index -> connection.get(String.valueOf(index).getBytes()));
+            return null;
+        });
+        System.out.println(objects);
+    }
+
+    @Test
+    void should_return_current_cache_type() {
+        System.out.println(cacheManager);
     }
 }
